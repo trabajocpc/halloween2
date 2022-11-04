87498121b84ed5ebc8459c336742d47f068a00fa
401 <401@academicos.cesde.edu.co>
2022-11-04 12:18:48 -0500

    se crea clase fiesta

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.example.clasesHalloween.Fiesta;
+import org.example.clasesHalloween.Invitado;
+import org.example.clasesHalloween.Lugar;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner entradaPorTeclado=new Scanner(System.in);
+        Invitado objetoInvitado = new Invitado();
+        Lugar objetoLugar = new Lugar();
+        Fiesta objetoFiesta = new Fiesta();
+
+        System.out.println("FIESTA");
+
+        System.out.println("1. Gastos de comida: ");
+        objetoFiesta.setCostosAlimentos(entradaPorTeclado.nextDouble());
+        System.out.println("El costo de alimetos fue: "+objetoFiesta.getCostosAlimentos());
+
+
+
+
+    }
+}
